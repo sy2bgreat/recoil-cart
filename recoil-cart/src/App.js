938654af55a3fbfab83b1c7e9c9fb77d9e0a8578
sy2bgreat@gmail.com <sdy2bgreat@gmail.com>
2021-01-19 10:19:57 +0900

    put data in the useEffect

--- recoil-cart/src/App.js
+++ recoil-cart/src/App.js
@@ -1,43 +1,53 @@
 import './App.css';
 import 'normalize.css';
-import { cart, cartState, addFood } from './store/atom'
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useState, useEffect } from 'react';
+import {  addFood, cartState } from './store/atom'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { Route, Router,  } from 'react-router-dom';
 import apple from './icons/apple.svg';
 import carrot from './icons/carrot.svg';
 import hotdog from './icons/hotdog.svg';
 import iceCream from './icons/icecream.svg';
 import pepperHot from './icons/pepper-hot.svg';
 import pizzaSlice from './icons/pizza-slice.svg';
+import { v4 as uuidv4 } from 'uuid';
 
-const ids = [
-  { name: 'Apple', src: apple, id:1},
-  { name: 'Carrot', src: carrot, id:2},
-  { name: 'Hotdog', src: hotdog, id:3},
-  { name: 'Ice cream', src: iceCream, id:4 },
-  { name: 'Pepper', src: pepperHot, id:5 },
-  { name: 'Pizze Slice!', src: pizzaSlice, id:6 },
-];
+const mockData = [
+  { name: 'Apple', src: apple, prc:1, id: uuidv4()},
+  { name: 'Carrot', src: carrot, prc:3, id:uuidv4()},
+  { name: 'Hotdog', src: hotdog, prc:5, id:uuidv4()},
+  { name: 'Ice cream', src: iceCream, prc:6, id:uuidv4()},
+  { name: 'Pepper', src: pepperHot, prc:8, id:uuidv4()},
+  { name: 'Pizze Slice!', src: pizzaSlice, prc:2, id:uuidv4()}
+]
+
 
 
 function App() {
-  const { totalQty } = useRecoilValue(cartState);
-  const setAddFood = useSetRecoilState(addFood);
+
+const [cart, setCart ] = useRecoilState(cartState)
+const setAddFood = useSetRecoilState(addFood);
+
+  useEffect(() => {
+    setCart(mockData)
+    console.log(cart);
+    
+  },[])
   return (
-    <section>
-      <div className="home-cart">Cart : {totalQty} </div>
-    <div className="home-card_container">
-    {ids.map((list, index) => (
+    <section>   
+    { <div className="home-card_container">
+    {cart.map((list, index) => (
     <div className="home-card" key={index}>
       <img src={list.src} className="home-card_img"></img>
-      <h3>{list.name}</h3> <h4>price: {list.id}</h4>
+      <h3>{list.name}</h3> <h4>price: ${list.prc}</h4>
       <button onClick={() => setAddFood(list.id)}>add</button>
     </div>
-    ))}
-
-    
+    ))} 
+    </div> }
+    <div>
     </div>
-    
   </section>
+ 
   );
 }
 
--- recoil-cart/src/store/atom.js
+++ recoil-cart/src/store/atom.js
@@ -1,20 +1,34 @@
 import { atom, selector, useRecoilState, useSetRecoilState } from 'recoil';
+// import data from './data'
+import { v4 as uuidv4 } from 'uuid';
 
-export const cart = atom({
-  key: 'cart',
-  default: [],
+export const cartState = atom({
+  key: 'cartState',
+  default: 
+  [],
+
 });
  
 
-export const cartState = selector({
-  key: 'cartState',
-  get: ({ get }) => {
-    const totalQty = get(cart).reduce((a, b) => a + b, 0);
-  return {
-    totalQty
-  }
-}
-});
+// export const cartState = selector({
+//   key: 'cartState',
+//   get: ({ get }) => {
+//     const totalCost = get(cart).reduce((a, b) => a + b, 0);
+   
+   
+//   return {
+//     totalCost,
+
+//   }
+// }
+// });
+
+
+
+// export const foodState = selector({
+//   key: 'foodState',
+//   get: ()=> fetchFood()
+// })
 
 // export const addFood = selector({
 //   key: 'addFood',
@@ -27,27 +41,10 @@ export const cartState = selector({
 
 export const addFood = selector({
   key: 'addFood',
-  set: ({ set, get }, newValue) => set(cart, [...get(cart),newValue]),
+  set: ({ set, get }, newValue) => set(cartState, [...get(cartState),newValue]),
+
+
 });
 
 
-// const cloneIndex = (items, id) => ({
-//     clone: items.map((item) => Object.assign({}, item)),
-//     index: items.findIndex((item) => item.id === id),
-//   });
-  
-  // export const AddItem = () => {
-  //   const [items, setItems] = useRecoilState(cart);
-  //   return (product) => {
-  //       const { clone, index } = cloneIndex(items, product.id);
-  //       if (index !== -1) {
-  //         clone[index].qty += 1;
-  //         setItems(clone);
-  //       } else { clone[index].qty -= 1;
-  //       setItems(clone);
-  //       }
-  //     }
-  // };
 
-
-  //export default { cart, addItem };
